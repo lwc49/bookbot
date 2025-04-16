db0d7e724bd6958b54e489c96d14f65fbba11909
lwc49 <lwc49a@gmail.com>
2025-04-16 21:22:48 +0800

    bug fixs+print in a report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_num, character_counter
+from stats import get_word_num, character_counter, sorting
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -10,7 +10,15 @@ def main():
     text=get_book_text(filepath)
     word_numbers=get_word_num(text)
     character_count=character_counter(text)
-    print(f"{word_numbers} words found in the document")
-    print(character_count)
-
+    sorted_list=sorting(character_count)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}")
+    print("----------- Word Count ----------")
+    print(f"Found {word_numbers} total words")
+    print("--------- Character Count -------")
+    for s in sorted_list:
+        if s["name"].isalpha() is True:
+            print(f"{s["name"]}: {s["num"]}")
+    print("============= END ===============")
+    
 main()
--- stats.py
+++ stats.py
@@ -9,7 +9,22 @@ def character_counter(text):
         if lowercase in character_count:
             character_count[lowercase]+=1
         else:
-            character_count[lowercase]=0
+            character_count[lowercase]=1
     return character_count
-            
+
+def sort_on(dict):
+    return dict["num"]
+
+def sorting(character_count):
+    sorted_list=[]
+    for key, value in character_count.items():
+        d={}
+        d["name"]=key
+        d["num"]=value
+        sorted_list.append(d)
+    sorted_list.sort(reverse=True, key=sort_on)
+
+
+    return sorted_list
+                
         
